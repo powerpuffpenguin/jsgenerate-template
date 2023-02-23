dd1393cfecaca03a0d05036f2429f26f2f68f8cc
zuiwuchang <zuiwuchang@gmail.com>
2023-02-23 06:25:43 +0000

    aes

--- angular/angular-15/src/app/dev/home/home.component.ts
+++ angular/angular-15/src/app/dev/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { aesDecrypt, aesEncrypt } from 'src/internal/aes';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  onClickAES() {
+
+    const val = aesEncrypt("ok", "12", "34")
+    console.log(val)
+    console.log(aesDecrypt(val, "12", "341"))
+  }
 }
